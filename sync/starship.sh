8c3858a83c5e02a35a1204faddb4ef14bc0a1417
kedarmd <dindekedar@gmail.com>
2026-03-20 23:12:33 +0530

    chore: fixed starship sync

--- sync/starship.sh
+++ sync/starship.sh
@@ -3,14 +3,15 @@ set -e
 
 REPO_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 STARSHIP_CONFIG_FILE="$HOME/.config/starship.toml"
-KMDOT_STARSHIP_CONFIG_FILE="$REPO_DIR/config/starship/starship.toml"
+KMDOT_STARSHIP_CONFIG_DIR="$HOME/.config/kmdot/starship"
 
 mkdir -p "$HOME/.config"
 
 rm -f "$STARSHIP_CONFIG_FILE"
 
-cp "$KMDOT_STARSHIP_CONFIG_FILE" "$HOME/.config/starship.toml"
+cp -r "$REPO_DIR/config/starship" "$KMDOT_STARSHIP_CONFIG_DIR"
 
+ln -sf "$KMDOT_STARSHIP_CONFIG_DIR/starship.toml" "$STARSHIP_CONFIG_FILE"
 echo "kmDot starship config synced!!!"
 
 
