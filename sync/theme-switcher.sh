51eea2b002ded19708701a0894deb176e43915a3
kedarmd <dindekedar@gmail.com>
2026-03-20 18:12:52 +0530

    chore: fixed theme switcher

--- sync/theme-switcher.sh
+++ sync/theme-switcher.sh
@@ -3,12 +3,15 @@ set -e
 
 REPO_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 KMDOT_THEME_SWITCHER_CONFIG_DIR="$HOME/.config/kmdot/theme-switcher"
+KMDOT_THEME_DIR="$HOME/.config/kmdot/themes"
 
 mkdir -p "$HOME/.config/kmdot"
 
 rm -rf "$KMDOT_THEME_SWITCHER_CONFIG_DIR"
+rm -rf "$KMDOT_THEME_DIR"
 
 cp -r "$REPO_DIR/theme-switcher" "$KMDOT_THEME_SWITCHER_CONFIG_DIR"
+cp -r "$REPO_DIR/themes" "$KMDOT_THEME_DIR"
 
 echo "kmDot theme-switcher config synced!!!"
 
