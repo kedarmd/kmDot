20338205c7e898306afe0528091ffb8c35ac6898
kedarmd <dindekedar@gmail.com>
2026-04-13 14:09:06 +0530

    chore: fixed snacks related errors

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -1,10 +1,10 @@
-local keymap = vim.api.nvim_set_keymap
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
+	---@type snacks.Config
 	opts = {
-		bigfile = { enabled = false },
+		bigfile = { enabled = true },
 		dashboard = {
 			preset = {
 				header = [[
@@ -18,80 +18,105 @@ return {
 			},
 		},
 		terminal = {
-			win = {
-				style = "terminal",
-			},
+			win = { style = "terminal" },
 		},
+		image = { enabled = true }, -- Enabled for your Ghostty setup
+		indent = { enabled = true },
+		input = { enabled = true }, -- Fixed: This resolves the vim.ui.input warnings
+		picker = { enabled = true }, -- Fixed: This resolves the vim.ui.select errors
+		notifier = { enabled = true },
 		lazygit = { enabled = true },
 		explorer = { enabled = true },
-		indent = { enabled = true },
-		input = { enabled = false },
-		picker = { enabled = true },
-		notifier = { enabled = true },
-		quickfile = { enabled = false },
+		-- Keep these disabled if you don't use them, but enabled is recommended for full features
+		quickfile = { enabled = true },
 		scope = { enabled = false },
 		scroll = { enabled = false },
 		statuscolumn = { enabled = false },
 		words = { enabled = false },
 	},
+	keys = {
+		-- Explorer & Git
+		{
+			"\\",
+			function()
+				Snacks.picker.explorer()
+			end,
+			desc = "Explorer",
+		},
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
 
-	-- keybings here...
-	keymap("n", "\\", ":lua Snacks.picker.explorer()<CR>", { noremap = true, silent = true }),
-	keymap("n", "<leader>lg", "<cmd>lua Snacks.lazygit()<CR>", { desc = "Lazygit", noremap = true, silent = true }),
-	keymap(
-		"n",
-		"<C-`>",
-		"<cmd>lua Snacks.terminal.toggle()<CR>",
-		{ desc = "Toggle Terminal", noremap = true, silent = true }
-	),
-	keymap(
-		"t",
-		"<C-`>",
-		"<cmd>lua Snacks.terminal.toggle()<CR>",
-		{ desc = "Toggle Terminal", noremap = true, silent = true }
-	),
+		-- Terminal (Normal & Terminal mode)
+		{
+			"<C-`>",
+			function()
+				Snacks.terminal.toggle()
+			end,
+			desc = "Toggle Terminal",
+		},
+		{
+			"<C-`>",
+			function()
+				Snacks.terminal.toggle()
+			end,
+			desc = "Toggle Terminal",
+			mode = "t",
+		},
 
-	-- Picker
-	keymap(
-		"n",
-		"<leader>sf",
-		"<cmd>lua Snacks.picker.files()<CR>",
-		{ desc = "[S]earch [F]iles", noremap = true, silent = true }
-	),
-	keymap(
-		"n",
-		"<leader><leader>",
-		"<cmd>lua Snacks.picker.buffers()<CR>",
-		{ desc = "[Search] [Buffers] existing", noremap = true, silent = true }
-	),
-	keymap(
-		"n",
-		"<leader>sw",
-		"<cmd>lua Snacks.picker.grep_word()<CR>",
-		{ desc = "[Search] current [W]ord", noremap = true, silent = true }
-	),
-	keymap(
-		"n",
-		"<leader>sg",
-		"<cmd>lua Snacks.picker.grep()<CR>",
-		{ desc = "[Search] by [G]rep", noremap = true, silent = true }
-	),
-	keymap(
-		"n",
-		"<leader>sd",
-		"<cmd>lua Snacks.picker.diagnostics()<CR>",
-		{ desc = "[Search] [D]iagnostics", noremap = true, silent = true }
-	),
-	keymap(
-		"n",
-		"<leader>sr",
-		"<cmd>lua Snacks.picker.resume()<CR>",
-		{ desc = "[Search] [R]esume", noremap = true, silent = true }
-	),
-	keymap(
-		"n",
-		"<leader>sb",
-		"<cmd>lua Snacks.picker.git_branches()<CR>",
-		{ desc = "[Search] [B]ranches", noremap = true, silent = true }
-	),
+		-- Picker / Search
+		{
+			"<leader>sf",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "[S]earch [F]iles",
+		},
+		{
+			"<leader><leader>",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "[Search] [Buffers]",
+		},
+		{
+			"<leader>sw",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "[Search] current [W]ord",
+		},
+		{
+			"<leader>sg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "[Search] by [G]rep",
+		},
+		{
+			"<leader>sd",
+			function()
+				Snacks.picker.diagnostics()
+			end,
+			desc = "[Search] [D]iagnostics",
+		},
+		{
+			"<leader>sr",
+			function()
+				Snacks.picker.resume()
+			end,
+			desc = "[Search] [R]esume",
+		},
+		{
+			"<leader>sb",
+			function()
+				Snacks.picker.git_branches()
+			end,
+			desc = "[Search] [B]ranches",
+		},
+	},
 }
