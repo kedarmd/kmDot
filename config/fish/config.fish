5bed4ecb1ed84d4916c359851df24a986575f660
kedarmd <dindekedar@gmail.com>
2026-05-14 23:08:18 +0530

    feat: added ability to connect to a remote ollma-server and masked the remote ip address here in fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,8 @@
 set -gx LANG en_IN.UTF-8
 set -gx LC_ALL en_IN.UTF-8
 
+source $__fish_config_dir/secrets.fish
+
 starship init fish | source
 set -U fish_greeting ""
 
@@ -14,3 +16,12 @@ fish_add_path $ANDROID_HOME/platform-tools
 set -gx JAVA_HOME /usr/lib/jvm/java-17-openjdk
 fish_add_path $JAVA_HOME/bin
 
+function ollama-remote
+    set -gx OLLAMA_HOST $OLLAMA_REMOTE_IP:11434
+    echo "Ollama is now pointing to the RTX 3060 server 🚀"
+end
+
+function ollama-local
+    set -e OLLAMA_HOST
+    echo "Ollama is back to local mode 🏠"
+end
