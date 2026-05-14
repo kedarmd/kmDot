3653c906f0472c1fae9bfe9f816827501ec1ff13
kedarmd <dindekedar@gmail.com>
2026-05-14 22:56:51 +0530

    chore: added android dev related package paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,14 @@ set -gx LC_ALL en_IN.UTF-8
 
 starship init fish | source
 set -U fish_greeting ""
+
+# Change the ANDROID_HOME path to the Arch default
+set -gx ANDROID_HOME /opt/android-sdk
+
+# Add the platform-tools to your path
+fish_add_path $ANDROID_HOME/platform-tools
+
+# Set Java Home for Arch
+set -gx JAVA_HOME /usr/lib/jvm/java-17-openjdk
+fish_add_path $JAVA_HOME/bin
+
